8ccb9143d9ff31061bc7e8e10f99e99fcef9d0b2
anilusul <anilusul@gmail.com>
2017-06-04 11:58:55 +0300

    dto and HelloWworldPage.jsp added.

---
+++ goldendaysweb/src/main/java/com/goldendays/dto/Dto.java
@@ -0,0 +1,33 @@
+package com.goldendays.dto;
+
+public class Dto {
+
+	private String name;
+	private String surName;
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
